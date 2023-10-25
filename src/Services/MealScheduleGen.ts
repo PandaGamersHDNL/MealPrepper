9eb3d148f9551861da826b78c140bcb8b248c524
panda jacobs <jokke.jacobs@gmail.com>
2023-10-25 03:34:08 +0200

    format meal gen service

--- src/Services/MealScheduleGen.ts
+++ src/Services/MealScheduleGen.ts
@@ -9,19 +9,19 @@ export class MealScheduleGenService {
         for (let i = 0; i < days; i++) {
             const recipe = recipes[this.RandomInt(recipes.length)];
             const dateOfMeal = this.AddDays(new Date, i);
-            meals.push({date: dateOfMeal, recipe: recipe});
+            meals.push({ date: dateOfMeal, recipe: recipe });
         }
         return meals;
     }
 
-    static AddDays(date: Date,days: number) {
+    static AddDays(date: Date, days: number) {
         date.setTime(date.getTime() + (days * 86400000)); //1 day = 86400000 ms
         return date;
     }
     //max not inclusive
     // min  inclusive
     static RandomInt(max: number, min = 0): number {
-        const random = Math.floor(Math.random() * max)+min;
+        const random = Math.floor(Math.random() * max) + min;
         return random
     }
 }
