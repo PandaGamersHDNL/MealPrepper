faa5ed901d163c87b546770d13823550f147bfd6
panda jacobs <jokke.jacobs@gmail.com>
2023-12-24 01:23:52 +0100

    fix ingr manager

--- src/Components/Dashboard/Ingredients/Manager.tsx
+++ src/Components/Dashboard/Ingredients/Manager.tsx
@@ -1,25 +1,28 @@
-import { Button, Group } from "@mantine/core";
+import { ActionIcon, Group, rem } from "@mantine/core";
 import { IngrList } from "./List";
 import { useDisclosure } from "@mantine/hooks";
 import { IngrModal } from "./Modal";
 import { useState } from "react";
 import { IIngredient, createEmptyIngr } from "../../../Interfaces/Ingredient";
+import { IconPlus } from "@tabler/icons-react";
 
 export function IngrManager() {
     const [opened, { open, close }] = useDisclosure(false);
-    const [edit, setEdit] = useState<IIngredient>(createEmptyIngr());
-    const openEdit = (ingr: IIngredient) => { setEdit(ingr); open(); }
+    const [editData, setEditData] = useState<IIngredient>(createEmptyIngr());
+    const openEdit = (recipe: IIngredient) => { setEditData(recipe); open(); }
+    //const [filter, setFilter] = useState("");
+    //<TextInput about="search your recipe" onChange={(v) => {setFilter(v.currentTarget.value) }} />
 
     return (
-        <>
-            <Group>
-                <Button onClick={open}>generate meals</Button>
-                <IngrModal opened={opened} Close={close} data={edit}/>
-                <IngrList openEdit={openEdit}/>
+        <div id="Recipes">
+            <Group id="RecipeHeader" justify="center">
+                <ActionIcon variant="subtle" color="red" onClick={() => openEdit(createEmptyIngr())}>
+                    <IconPlus style={{ width: rem(16), height: rem(16) }} stroke={1.5} />
+                </ActionIcon>
             </Group>
-        </>
+            <IngrList openEdit={openEdit} />
+            <IngrModal opened={opened} Close={close} data={editData} />
+        </div>
     )
 }
-
-
     
