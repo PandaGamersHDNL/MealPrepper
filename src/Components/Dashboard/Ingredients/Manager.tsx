7ec6627fb02d63e5d2c8b20ae398eed8ae78aaad
Jokke <jokke.jacobs@gmail.com>
2024-12-22 01:56:06 +0100

    formatting the ingredients manager + "+" icon

--- src/Components/Dashboard/Ingredients/Manager.tsx
+++ src/Components/Dashboard/Ingredients/Manager.tsx
@@ -1,18 +1,38 @@
-import { Button, Group } from "@mantine/core";
+import { ActionIcon, Button, Group, rem } from "@mantine/core";
 import { IngredientList } from "./List";
 import { IngredientsModal } from "./FormModal";
 import { useDisclosure } from "@mantine/hooks";
-import { CreateEmptyIngredient, IIngredient } from "../../../Interfaces/Ingredient";
+import {
+    CreateEmptyIngredient,
+    IIngredient,
+} from "../../../Interfaces/Ingredient";
 import { useState } from "react";
+import { IconPlus } from "@tabler/icons-react";
 
 export function IngredientsManager() {
     const [opened, { open, close }] = useDisclosure(false);
-    const [editData, setEditData] = useState<IIngredient>(CreateEmptyIngredient());
-    const openEdit = (ingredient: IIngredient) => { setEditData(ingredient); open(); }
+    const [editData, setEditData] = useState<IIngredient>(
+        CreateEmptyIngredient()
+    );
+    const openEdit = (ingredient: IIngredient) => {
+        setEditData(ingredient);
+        open();
+    };
     return (
         <Group>
-            <Button onClick={() => openEdit(CreateEmptyIngredient())}></Button>
-            <IngredientList openEdit={openEdit}/>
+            <Group id="IngredientHeader" justify="center">
+                <ActionIcon
+                    variant="subtle"
+                    color="red"
+                    onClick={() => openEdit(CreateEmptyIngredient())}
+                >
+                    <IconPlus
+                        style={{ width: rem(16), height: rem(16) }}
+                        stroke={1.5}
+                    />
+                </ActionIcon>
+            </Group>
+            <IngredientList openEdit={openEdit} />
             <IngredientsModal close={close} opened={opened} data={editData} />
         </Group>
     );
