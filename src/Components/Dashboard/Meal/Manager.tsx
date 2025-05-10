4b0db7674da64fc49e0d99d4d3f62949db23cd10
Jokke <jokke.jacobs@gmail.com>
2025-05-10 17:59:57 +0200

    clear all buttons

--- src/Components/Dashboard/Meal/Manager.tsx
+++ src/Components/Dashboard/Meal/Manager.tsx
@@ -3,14 +3,19 @@ import { MealModal } from "./Modal";
 import { useDisclosure } from "@mantine/hooks";
 import { MealList } from "./List";
 import { GroceryListManager } from "../GroceryList/Manager";
+import { useContext } from "react";
+import { UserDataCTX } from "../../../App";
 
 export function MealManager() {
     const [opened, { open, close }] = useDisclosure(false);
+    const DataManager = useContext(UserDataCTX)!.dataManager;
+    //TODO update meals after deletion here so the list updates
     return (
         <Stack >
             <Group grow={true}>
                 <Button onClick={open}>generate meals</Button>
                 <GroceryListManager />
+                <Button onClick={()=> {DataManager.DeleteAllMeals();}}>Clear all</Button>
             </Group>
             <MealList />
             <MealModal opened={opened} Close={close} />
