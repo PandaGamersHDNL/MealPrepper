011926c38850b51c728bc32256ddb2b97e25c9d0
Jokke <jokke.jacobs@gmail.com>
2025-05-31 17:35:38 +0200

    no dupes

--- src/Components/Dashboard/Meal/Modal.tsx
+++ src/Components/Dashboard/Meal/Modal.tsx
@@ -6,7 +6,7 @@ import { useContext } from "react";
 import { UserDataCTX } from "../../../App";
 
 export function MealModal(props: { opened: boolean, Close: () => void }) {
-    const form = useForm<{ length: number, startDate: Date }>({ initialValues: { length: 7, startDate: new Date() } });
+    const form = useForm<{ length: number, startDate: Date, noDupesDays: number }>({ initialValues: { length: 7, startDate: new Date(), noDupesDays: 7 } });
     const userDataCtx = useContext(UserDataCTX)!;
     
     return <>
@@ -14,13 +14,16 @@ export function MealModal(props: { opened: boolean, Close: () => void }) {
             <Form form={form}>
                 <NumberInput {...form.getInputProps("length")} label={"days"} />
                 <DateInput {...form.getInputProps("startDate")} />
+                <NumberInput {...form.getInputProps("noDupesDays")} label="no dublicates" description="no dublicates for this amount of days" />
+
                 <Group><Button onClick={() => {
                     try {
 
                         const meals = MealScheduleGenService.GenMeals(
                             userDataCtx.dataManager, 
                             form.values.length, 
-                            form.values.startDate
+                            form.values.startDate,
+                            form.values.noDupesDays
                         );
                         userDataCtx.setUserData({Meals: meals})
                         props.Close();
